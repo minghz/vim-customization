00a52efe241f185b235c773fe837fa2b1179c810
Minghua Zhao <minghz42@gmail.com>
2017-11-23 12:17:16 -0500

    add scripts to back-up and load env

---
+++ backup.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script copies the current vim setup into this repo
+# TODO: back-up plugins within submodules
+cp ~/.vimrc vimrc
+cp ~/.gvimrc gvimrc
---
+++ load.sh
@@ -0,0 +1,6 @@
+#!/bin/bash
+
+# This script copies the stuff here into the working directory
+# TODO: reload plugins within pathogen
+cp vimrc ~/.vimrc
+cp gvimrc ~/.gvimrc
